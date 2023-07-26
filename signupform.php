d8184e22bc749017d9019a59556a7adf130474a3
LevanNamicheishvili <levannnamicheishvili@gmail.com>
2023-07-26 03:31:26 -0700

    feat

---
+++ signupform.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <!-- Signup Form -->
+    <form id="signup-form" action="signup.php" method="post">
+        <h2>SIGN UP</h2>
+        <?php if (isset($_GET['signup_error'])) { ?>
+            <p class="error"><?php echo $_GET['signup_error']; ?></p>
+        <?php } ?>
+
+        <label>User Name</label>
+        <input type="text" name="uname" placeholder="User Name">
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password">
+
+        <!-- Error Message Div -->
+        <div id="error-message" class="error" style="display: none;">All fields are required for signup</div>
+
+        <button type="submit" name="signup" id="signup-button">Sign Up</button>
+    </form>
+<script src="./js/signupf.js"></script>
+</body>
+</html>
