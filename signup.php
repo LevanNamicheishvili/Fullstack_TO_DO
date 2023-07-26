e204a0b38806320f5811b7c75fdc1aa9f265e55b
LevanNamicheishvili <levannnamicheishvili@gmail.com>
2023-07-26 05:32:37 -0700

    bug/fix

--- signup.php
+++ signup.php
@@ -3,43 +3,30 @@ session_start();
 include "db_conn.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (isset($_POST['uname']) && isset($_POST['password'])) {
-        // Function to validate user input
-        function validate($data)
-        {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-        }
-
-        $uname = validate($_POST['uname']);
-        $pass = validate($_POST['password']);
+    if (isset($_POST['uname'], $_POST['password'])) {
+        $uname = htmlspecialchars(trim($_POST['uname']));
+        $pass = htmlspecialchars(trim($_POST['password']));
 
         if (empty($uname) || empty($pass)) {
-            header("Location: signup.php?signup_error=All fields are required for signup");
-            exit();
+            redirectWithMessage("signup.php", "All fields are required for signup");
+        }
+
+        // Check if the username is already taken
+        $sql = "SELECT * FROM users WHERE user_name='$uname'";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            redirectWithMessage("signup.php", "Username already taken. Please choose a different one.");
+        }
+
+        // Insert the new user into the database
+        $sql = "INSERT INTO users (user_name, password) VALUES ('$uname', '$pass')";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            redirectWithMessage("index.php", "Signup successful. You can now login.", "signup_success");
         } else {
-            // Check if the username is already taken
-            $sql = "SELECT * FROM users WHERE user_name='$uname'";
-            $result = mysqli_query($conn, $sql); // $conn is the connection variable from db_conn.php
-
-            if (mysqli_num_rows($result) > 0) {
-                header("Location: signup.php?signup_error=Username already taken. Please choose a different one.");
-                exit();
-            } else {
-                // Insert the new user into the database
-                $sql = "INSERT INTO users (user_name, password) VALUES ('$uname', '$pass')";
-                $result = mysqli_query($conn, $sql); // $conn is the connection variable from db_conn.php
-
-                if ($result) {
-                    header("Location: index.php?signup_success=Signup successful. You can now login.");
-                    exit();
-                } else {
-                    header("Location: signup.php?signup_error=Signup failed. Please try again later.");
-                    exit();
-                }
-            }
+            redirectWithMessage("signup.php", "Signup failed. Please try again later.", "signup_error");
         }
     } else {
         header("Location: signup.php");
@@ -47,6 +34,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 } else {
     header("Location: signup.php");
+    exit();
+}
+
+function redirectWithMessage($location, $message, $param = "") {
+    $url = $location;
+    if (!empty($param)) {
+        $url .= "?" . $param . "=" . urlencode($message);
+    }
+    header("Location: " . $url);
     exit();
 }
 ?>
